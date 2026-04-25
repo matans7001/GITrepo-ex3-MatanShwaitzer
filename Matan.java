65a88242d42f5d71f2b49abb6076675a0a7cf296
matans7001 <matans7001@gmail.com>
2026-04-25 14:37:58 +0300

    Updated Name Print

--- Matan.java
+++ Matan.java
@@ -1,7 +1,7 @@
 public class Matan{
 	public static void main(String[] args){
 		for(int i = 1; i <= 10; i++){
-			System.out.println(i + ". Matan");
+			System.out.println("Name: Matan");
 		}
 	}
 }
